dfc37ebbc8776d6a325118a5f3ef05bfec259a5a
Mert Erdem Güneş <123267437+merterdemgunes@users.noreply.github.com>
2024-03-02 12:50:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path'; // Import the 'path' module
+import pkg from './package.json';
 
 // Define assetUrlOptions
 const assetUrlOptions = {
-  base: options.devServer.config.base + path.relative(options.root, path.dirname(filename))
+  base: pkg.homepage, // Set the base option to the homepage specified in package.json
 };
 
 export default defineConfig({
   base: assetUrlOptions.base, // Set the base option dynamically
   plugins: [react()],
 });
-
